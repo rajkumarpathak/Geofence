e2b49952b8484743828cd4d8dc81b7905e4b4b8a
rajkumarpathak879@gmail.com <pvijay@talisma.com>
2023-02-08 11:38:53 +0530

    added new file

---
+++ mapcalculation.js
@@ -0,0 +1,24 @@
+//Random lat long
+var lat1 = 18.9441296;
+var lon1 = 72.8305983;
+var lat2 = 18.93739;
+var lon2 = 72.83468;
+
+//Calculate two lat long in meters
+var distanceInMeters = distance(lat1, lon1, lat2, lon2);
+console.log(distanceInMeters);
+
+function distance(lat1, lon1, lat2, lon2) {
+  var R = 6371e3; // radius of Earth in meters
+  var phi1 = lat1 * Math.PI / 180;
+  var phi2 = lat2 * Math.PI / 180;
+  var deltaPhi = (lat2-lat1) * Math.PI / 180;
+  var deltaLambda = (lon2-lon1) * Math.PI / 180;
+
+  var a = Math.sin(deltaPhi/2) * Math.sin(deltaPhi/2) +
+          Math.cos(phi1) * Math.cos(phi2) *
+          Math.sin(deltaLambda/2) * Math.sin(deltaLambda/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  return R * c;
+}
